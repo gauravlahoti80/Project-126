b8fce85c0d7a103b5b8989fd730fe7bd3045bc5b
Gaurav Lahoti <73291209+gauravlahoti80@users.noreply.github.com>
2021-10-26 18:11:07 +0530

    Add files via upload

---
+++ power.py
@@ -0,0 +1,12 @@
+'''for i in range(0,5):
+    print(10**i)'''
+
+for i in range(1, 11):
+    print(i*i)
+
+for i in range(1, 11):
+    print((i*i)-1)
+
+
+#4,9,16,25, 36 ....
+#3 , 8 , 15 , 24 , 35 , 48 ..
